40b8ce5feb3022e410859c203bccec14e313b3bc
tamana007 <tamana.efatwira1@gmail.com>
2023-10-23 13:05:06 -0700

    g

--- Navbar.js
+++ Navbar.js
@@ -4,7 +4,23 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return (
+    <>
+    <nav className='nav-header'>
+      <div>Logo goes here</div>
+      <div className='links-container'>
+        <ul className='links-container'>
+          {links.map((item)=>{return <li>{item.text}</li>})}
+          
+        
+          </ul>
+        
+        </div>
+      <div>social media goes here</div>
+    </nav>
+    </>
+  )
+
 }
 
 export default Navbar
